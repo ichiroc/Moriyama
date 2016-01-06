7fe92a63bf04999efdc812209ae3249770062312
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-01-07 05:50:45 +0900

    Change cancel button to done button.

--- MoriyamaKeyboard/Classes/MRYDayViewController.swift
+++ MoriyamaKeyboard/Classes/MRYDayViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MRYDayViewController: UIViewController {
     var views : [String: UIView]!
-    var cancelButton : UIButton!
+    var doneButton : UIButton!
     var insertButton : UIButton!
     var monthlyView : MRYMonthlyCalendarCollectionView?
     var timelineScrollView : UIScrollView!
@@ -19,6 +19,7 @@ class MRYDayViewController: UIViewController {
     let hourlyHeight = 40.0
     var timeline : UIView!
     var timelineSideBar : UIView!
+    let timelineSidebarWidth : CGFloat = 25.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,25 +31,34 @@ class MRYDayViewController: UIViewController {
         timelineScrollView = UIScrollView()
         timelineScrollView.translatesAutoresizingMaskIntoConstraints = false
         
-        let width = self.view.frame.width - 32 - 25
-        let height = CGFloat(24 * hourlyHeight)
-        timeline = UIView(frame: CGRectMake(25,0,width,height))
+        let timelineWidth = self.view.frame.width - 32.0 - timelineSidebarWidth
+        let timelineHeight = CGFloat(24 * hourlyHeight)
+        timeline = UIView(frame: CGRectMake(timelineSidebarWidth,0,timelineWidth,timelineHeight))
        
-        timelineSideBar = UIView(frame: CGRectMake(0,0,25,height))
+        timelineSideBar = UIView(frame: CGRectMake(0,0,25,timelineHeight))
         timelineSideBar.backgroundColor = UIColor.whiteColor()
         
         layoutTimeline()
         
-        cancelButton = MRYKeyboardButton(title: "Cancel", text: nil, backgroundColor: UIColor.whiteColor(), titleColor: UIColor.blueColor(), action: self.dismissSelf)
+        doneButton = MRYKeyboardButton(title: "Done",
+            text: nil,
+            backgroundColor: UIColor.whiteColor(),
+            titleColor: UIColor.blueColor(),
+            action: self.dismissSelf)
         
         var titleText = "Insert"
         if let date = currentDate{
             titleText = formatter.stringFromDate(date)
         }
-        insertButton = MRYKeyboardButton(title: titleText, text: nil, backgroundColor: UIColor.blueColor(), titleColor: UIColor.whiteColor(), action: { self.insert()})
+        insertButton = MRYKeyboardButton(
+            title: titleText,
+            text: nil,
+            backgroundColor: UIColor.blueColor(),
+            titleColor: UIColor.whiteColor(),
+            action: { self.insert()})
         
         views = ["timeline": timeline,
-            "cancel": cancelButton,
+            "cancel": doneButton,
             "insert" : insertButton,
             "timelineScroll": timelineScrollView]
         
@@ -61,14 +71,13 @@ class MRYDayViewController: UIViewController {
         timelineScrollView.addSubview(timeline)
         timelineScrollView.addSubview(timelineSideBar)
         timelineScrollView.contentSize = CGSizeMake(timeline.frame.width, timeline.frame.height)
-       
         
         for (var i = 1.0 ; i < 24 ; i++) { // 0時の描画はしない
             let hour = UIView(frame: CGRectMake(0, CGFloat(i * hourlyHeight), timeline.frame.width, 1 ))
             hour.backgroundColor = UIColor.lightGrayColor()
             timeline.addSubview(hour)
             
-            let timeLabel = UILabel(frame: CGRectMake(0, CGFloat(i * hourlyHeight - (hourlyHeight / 2) ), 25, CGFloat(hourlyHeight)))
+            let timeLabel = UILabel(frame: CGRectMake(0, CGFloat(i * hourlyHeight - (hourlyHeight / 2) ), timelineSidebarWidth, CGFloat(hourlyHeight)))
             timeLabel.text = "\(Int(i))"
             timeLabel.font.fontWithSize(11.0)
             timeLabel.adjustsFontSizeToFitWidth = true
@@ -82,7 +91,7 @@ class MRYDayViewController: UIViewController {
     func constraintsSubviews() -> [NSLayoutConstraint]{
         var constraints : [NSLayoutConstraint] = []
         
-        self.view.addSubview(cancelButton)
+        self.view.addSubview(doneButton)
         self.view.addSubview(timelineScrollView)
         
         let vertical = NSLayoutConstraint.constraintsWithVisualFormat(
