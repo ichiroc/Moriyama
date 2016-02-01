5ab52beefe06344f7243c8a07782eeb662860485
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-02-02 05:13:10 +0900

    Fix time detail table view layout bug.

--- MoriyamaKeyboard/Classes/MRYTimeDetailTableViewController.swift
+++ MoriyamaKeyboard/Classes/MRYTimeDetailTableViewController.swift
@@ -21,16 +21,7 @@ class MRYTimeDetailTableViewController: MRYAbstractMainViewController {
     }
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        let doneButton = MRYKeyboardButton(title: "Done",  action:  { self.popViewController() } )
-        self.view.addSubview(doneButton)
-        let tableView = MRYTimeDetailTableView(event: _event! )
-        self.view.addSubview(tableView)
-        views = [ "done": doneButton,
-            "table": tableView]
+    override func viewWillAppear(animated: Bool) {
         let tableHorizonalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
             "H:|[table]|",
             options: NSLayoutFormatOptions(rawValue: 0),
@@ -39,11 +30,25 @@ class MRYTimeDetailTableViewController: MRYAbstractMainViewController {
         self.view.addConstraints(tableHorizonalConstraints)
         
         let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|[done(35)]-[table]|",
+            "V:|[back(35)]-[table]|",
             options: [.AlignAllCenterX, .AlignAllLeading, .AlignAllTrailing] ,
             metrics: METRICS,
             views: views)
         self.view.addConstraints(verticalConstraints)
+//        self.view.layoutIfNeeded()
+        super.viewWillAppear(animated)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+
+        // Do any additional setup after loading the view.
+        let backButton = MRYKeyboardButton(title: "Back",titleColor: UIColor.blueColor() , action:  { self.popViewController() } )
+        self.view.addSubview(backButton)
+        let tableView = MRYTimeDetailTableView(event: _event! )
+        self.view.addSubview(tableView)
+        views = [ "back": backButton,
+            "table": tableView]
         
     }
     
