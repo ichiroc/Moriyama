7a4d80548fda6fb452c7826bada9c63e4a51e53d
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-02-10 07:51:38 +0900

    Add MRYAllDayEventView.

---
+++ MoriyamaKeyboard/Classes/MRYAllDayEventView.swift
@@ -0,0 +1,87 @@
+//
+//  MRYAllDayEventView.swift
+//  Moriyama
+//
+//  Created by Ichiro on 2016/02/09.
+//  Copyright © 2016年 Ichiro. All rights reserved.
+//
+
+import UIKit
+
+class MRYAllDayEventView: UIView {
+
+    var allDayEventViews : [String: MRYEventView] = [:]
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    init(allDayEvents:[MRYEvent], hourlyHeight: CGFloat, timelineSidebarWidth : CGFloat, viewController: MRYDayViewController){
+        super.init(frame: CGRectZero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let sidebarView = UIView()
+        sidebarView.translatesAutoresizingMaskIntoConstraints = false
+        sidebarView.backgroundColor = UIColor.whiteColor()
+        let allDayEventContainerView = UIView()
+        allDayEventContainerView.translatesAutoresizingMaskIntoConstraints = false
+        allDayEventContainerView.backgroundColor = UIColor.whiteColor()
+        self.addSubview(allDayEventContainerView)
+        
+        let allDayLabel = UILabel()
+        allDayLabel.text = "All Day"
+        allDayLabel.textAlignment = .Center
+        allDayLabel.textColor = UIColor.grayColor()
+        sidebarView.addSubview(allDayLabel)
+        allDayLabel.sizeToFit()
+        allDayLabel.font = allDayLabel.font.fontWithSize(11)
+        self.addSubview(sidebarView)
+        
+        let allDayViews = ["sidebar" : sidebarView,
+        "allDayEventContainer": allDayEventContainerView]
+        
+        var vfl = "|"
+        var i = 0
+        allDayEvents.filter({ $0.allDay }).forEach({
+            let eventView = MRYEventView(frame: CGRectZero, event: $0, hourlyHeight: hourlyHeight, viewController: viewController)
+            eventView.translatesAutoresizingMaskIntoConstraints = false
+            allDayEventContainerView.addSubview(eventView)
+            allDayEventViews["e\(i)"] = eventView
+            if(i == 0 ){
+                vfl += "[e\(i)]"
+            }else{
+                vfl += "[e\(i)(==e0)]"
+            }
+            i++
+        })
+        vfl += "|"
+       
+        if(allDayEventViews.count > 0){
+            allDayEventContainerView.addConstraints(
+                NSLayoutConstraint.constraintsWithVisualFormat(
+                    "V:|[e0]|",
+                    options: NSLayoutFormatOptions(rawValue: 0),
+                    metrics: nil,
+                    views: allDayEventViews)
+            )
+            allDayEventContainerView.addConstraints(
+                NSLayoutConstraint.constraintsWithVisualFormat(
+                    vfl,
+                    options: [.AlignAllBottom, .AlignAllTop],
+                    metrics: nil,
+                    views: allDayEventViews)
+            )
+        }
+        let hConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[sidebar(\(timelineSidebarWidth))][allDayEventContainer]|", options: [.AlignAllTop, .AlignAllBottom], metrics: nil, views: allDayViews)
+        let vConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[sidebar]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: allDayViews)
+        self.addConstraints(hConstraints)
+        self.addConstraints(vConstraints)
+        
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- MoriyamaKeyboard/Classes/MRYDayViewController.swift
+++ MoriyamaKeyboard/Classes/MRYDayViewController.swift
@@ -70,7 +70,7 @@ class MRYDayViewController: MRYAbstractMainViewController {
         self.view.backgroundColor = UIColor.lightGrayColor()
         timelineScrollView = timelineView()
         
-        allDayEventView = buildAllDayEventView()
+        allDayEventView = MRYAllDayEventView(allDayEvents: events, hourlyHeight: hourlyHeight, timelineSidebarWidth: timelineSidebarWidth, viewController: self)
         accessoryKeyView = MRYDayViewAccessoryView(date: currentDate, viewController: self)
         self.view.addSubview(allDayEventView)
         managedSubViews = [
@@ -129,68 +129,6 @@ class MRYDayViewController: MRYAbstractMainViewController {
         
     }
 
-    private func buildAllDayEventView() -> UIView{
-        let allDayView = UIView()
-        allDayView.translatesAutoresizingMaskIntoConstraints = false
-        let sidebarView = UIView()
-        sidebarView.translatesAutoresizingMaskIntoConstraints = false
-        sidebarView.backgroundColor = UIColor.whiteColor()
-        let allDayEventContainerView = UIView()
-        allDayEventContainerView.translatesAutoresizingMaskIntoConstraints = false
-        allDayEventContainerView.backgroundColor = UIColor.whiteColor()
-        allDayView.addSubview(allDayEventContainerView)
-        
-        let allDayLabel = UILabel()
-        allDayLabel.text = "All Day"
-        allDayLabel.textAlignment = .Center
-        allDayLabel.textColor = UIColor.grayColor()
-        sidebarView.addSubview(allDayLabel)
-        allDayLabel.sizeToFit()
-        allDayLabel.font = allDayLabel.font.fontWithSize(11)
-        allDayView.addSubview(sidebarView)
-        
-        allDayViews = ["sidebar" : sidebarView,
-        "allDayEventContainer": allDayEventContainerView]
-        
-        var vfl = "|"
-        var i = 0
-        allDayEvents.forEach({
-            let eventView = MRYEventView(frame: CGRectZero, event: $0, hourlyHeight: hourlyHeight, viewController: self)
-            eventView.translatesAutoresizingMaskIntoConstraints = false
-            allDayEventContainerView.addSubview(eventView)
-            allDayEventViews["e\(i)"] = eventView
-            if(i == 0 ){
-                vfl += "[e\(i)]"
-            }else{
-                vfl += "[e\(i)(==e0)]"
-            }
-            i++
-        })
-        vfl += "|"
-       
-        if(allDayEventViews.count > 0){
-            allDayEventContainerView.addConstraints(
-                NSLayoutConstraint.constraintsWithVisualFormat(
-                    "V:|[e0]|",
-                    options: NSLayoutFormatOptions(rawValue: 0),
-                    metrics: nil,
-                    views: allDayEventViews)
-            )
-            allDayEventContainerView.addConstraints(
-                NSLayoutConstraint.constraintsWithVisualFormat(
-                    vfl,
-                    options: [.AlignAllBottom, .AlignAllTop],
-                    metrics: nil,
-                    views: allDayEventViews)
-            )
-        }
-        let hConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[sidebar(\(timelineSidebarWidth))][allDayEventContainer]|", options: [.AlignAllTop, .AlignAllBottom], metrics: nil, views: allDayViews)
-        let vConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|[sidebar]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: allDayViews)
-        allDayView.addConstraints(hConstraints)
-        allDayView.addConstraints(vConstraints)
-        
-        return allDayView
-    }
     private func layoutEventViews(){
         // set default frame
         events.filter({ return !$0.allDay }).forEach({
