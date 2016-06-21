50926926173b0f0b27bdd3461a750118909649d2
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-06-21 22:45:33 +0900

    Add a review button.

--- Moriyama/ViewController.swift
+++ Moriyama/ViewController.swift
@@ -11,11 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var supportButton: UIButton!
-    private let supportURL : NSURL! = NSURL(string: "https://twitter.com/apptboard")
+    @IBOutlet var reviewButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         supportButton.addTarget(self, action: #selector(ViewController.tappedSupportButton), forControlEvents: .TouchUpInside)
+        reviewButton.addTarget(self, action: #selector(ViewController.tappedReviewButton), forControlEvents: .TouchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,11 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func tappedSupportButton(){
-        let url = NSURL(string: "https://twitter.com/apptboard")
+    func tappedReviewButton(){
+        let url : NSURL! = NSURL(string: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=1087163548")
         let app = UIApplication.sharedApplication()
-        if(app.canOpenURL(supportURL)){
-            app.openURL(url!)
+        if(app.canOpenURL(url)){
+            app.openURL(url)
+        }
+    }
+    
+    func tappedSupportButton(){
+        let url : NSURL! = NSURL(string: "https://twitter.com/apptboard")
+        let app = UIApplication.sharedApplication()
+        if(app.canOpenURL(url)){
+            app.openURL(url)
         }
     }
 
